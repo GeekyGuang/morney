374e9dd5f3031c28f2299f5d79501d3b7557b049
travis <travis.x.wong@gmail.com>
2020-09-15 16:01:08 +0800

    添加flex样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>Money.vue
+  <div class="money-wrapper">
+    <div class="money">
+      Money.vue
+    </div>
     <Nav/>
   </div>
 
@@ -12,5 +15,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.money {
+  border: 1px solid green;
+  overflow: auto;
+  flex-grow: 1;
+}
 
+.money-wrapper{
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 </style>
