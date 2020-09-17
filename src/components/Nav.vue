e03ba94c35729b479bfcc428b7c77a9c6b3455fe
travis <travis.x.wong@gmail.com>
2020-09-17 10:15:43 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -21,6 +21,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
 nav{
   display: flex;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -41,7 +43,7 @@ nav{
   }
 
   > .item.selected{
-    color: #f60;
+    color: $color-highlight;
   }
 }
 
