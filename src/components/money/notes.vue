d36bb210152c12995d8da54240f3def7afadc025
travis <travis.x.wong@gmail.com>
2020-09-17 19:59:16 +0800

    模块化

---
+++ src/components/money/notes.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="这里输入备注">
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'notes'
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  display: flex;
+  padding-left: 16px;
+  align-items: center;
+
+  > .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 64px;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+    flex-grow: 1;
+  }
+}
+</style>
---
+++ src/components/money/type.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul class="type">
+      <li class="selected">支出</li>
+      <li>收入</li>
+    </ul>
+</template>
+
+<script lang="ts">
+export default {
+name: "type"
+}
+</script>
+
+<style lang="scss" scoped>
+.type {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+
+  li {
+    width: 100%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected {
+      &::before {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        height: 4px;
+        width: 100%;
+        background: #333;
+      }
+    }
+  }
+
+}
+</style>
