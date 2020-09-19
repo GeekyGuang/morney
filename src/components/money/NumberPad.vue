6b93ff11afd4d754d8dcc0758d951b1125a95aa1
travis <travis.x.wong@gmail.com>
2020-09-19 15:09:04 +0800

    完成numberPad组件

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,33 +1,70 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
+      <button @click="outputChange">1</button>
+      <button @click="outputChange">2</button>
+      <button @click="outputChange">3</button>
+      <button @click="deleteLast">删除</button>
+      <button @click="outputChange">4</button>
+      <button @click="outputChange">5</button>
+      <button @click="outputChange">6</button>
+      <button @click="clear">清空</button>
+      <button @click="outputChange">7</button>
+      <button @click="outputChange">8</button>
+      <button @click="outputChange">9</button>
       <button class="OK">OK</button>
-      <button class="zero">0</button>
-      <button>.</button>
+      <button @click="outputChange" class="zero">0</button>
+      <button @click="outputChange">.</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-name: "numberPad"
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output = '0';
+
+  outputChange(event: MouseEvent) {
+    const input = event.target.textContent;
+    if (this.output.length === 16) {return;}
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return
+    }
+
+    if (this.output.indexOf('.') >= 0 && input === '.') {
+      return;
+    }
+
+    this.output += input;
+  }
+
+  deleteLast(){
+    if(this.output.length === 1) {
+      this.output = '0'
+    } else {
+      this.output = this.output.slice(0,-1)
+    }
+  }
+
+  clear(){
+    this.output = '0'
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   .output {
     @extend %innerShadow;
